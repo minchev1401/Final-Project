dc756837abaf0bc4d6d11dfab7a52390f837ccc9
minchev1401 <vvasev020@gmail.com>
2024-03-19 08:53:58 +0200

    1

--- Classes.py
+++ Classes.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from sqlalchemy import create_engine, Column, Integer, String, Date, Float
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,declarative_base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
